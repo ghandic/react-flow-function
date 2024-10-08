b4f7343b1f70ce593a19e6993962cc632440f59c
ghandic <andrewchallis@hotmail.co.uk>
2024-10-08 23:49:07 +1100

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
